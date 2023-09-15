c917a90bafba8b1420c8fa48816991d442189b1d
LMR <dlaudfuf33@gmai.com>
2023-09-15 17:47:08 +0900

    Initial commit

---
+++ src/frontend/src/App.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+function App() {
+  const [data, setData] = useState('')
+
+  useEffect(() => {
+    axios.get('/api/data')
+        .then(res => setData(res.data))
+        .catch(err => console.log(err))
+  }, []);
+
+  return (
+      <div>
+        받아온 값 : {data}
+      </div>
+  );
+}
+ 
+export default App;
---
+++ src/main/java/com/example/demo/BoardController.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BoardController {
+
+    @GetMapping("/api/data")
+    public String test() {
+        return "Hello, world!";
+    }
+}
