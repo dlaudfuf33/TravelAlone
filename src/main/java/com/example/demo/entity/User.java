7d88534aed63db9be94ff33135376b070b4e10da
LMR <dlaudfuf33@gmai.com>
2023-11-04 15:23:35 +0900

    ttt

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -24,7 +24,8 @@ public class User {
     @NotBlank(message = "비밀번호는 필수입니다.")
     @Size(min = 8, message = "비밀번호는 최소 8자 이상이어야 합니다.")
     private String password;
-//test
+//testss
+
     @Column(unique = true)
 //    @Email(message = "유효한 이메일 형식이어야 합니다.")
     private String email;
